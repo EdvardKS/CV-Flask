2a00c1523440e0aa85ce7010e8ce6a59a3e34175
Edvard Khacahtryan Sahakyan <118193785+EdvardKS@users.noreply.github.com>
2024-10-01 15:59:09 +0000

    Reorganización Main.py

--- main.py
+++ main.py
@@ -26,11 +26,10 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'mysecret')
 # Configurar el servidor de correo electrónico
 SMTP_USERNAME = os.getenv('MAIL_USERNAME')  
 SMTP_PASSWORD = os.getenv('MAIL_PASSWORD')  
-SENDER_EMAIL = 'info@edvardks.com'
-SMTP_SERVER = 'ssl0.ovh.net'
-SMTP_PORT = 587
-RECIPIENT_EMAIL = 'info@edvardks.com'
+SMTP_SERVER = os.getenv('SMTP_SERVER')  
+SMTP_PORT = os.getenv('SMTP_PORT')  
 
+print("\n",SMTP_USERNAME, SMTP_SERVER, SMTP_PORT, "\n")
 ####################################################################################################
 ####################################################################################################
 ####################################################################################################
@@ -48,8 +47,8 @@ def submit_contact():
             return jsonify({'status': 'error', 'message': 'Missing required fields'}), 400
         logging.info(f"Attempting to send email from {email}")
         msg = MIMEMultipart()
-        msg['From'] = SENDER_EMAIL  # Use the SENDER_EMAIL as the sender
-        msg['To'] = RECIPIENT_EMAIL
+        msg['From'] = SMTP_USERNAME 
+        msg['To'] = SMTP_USERNAME
         msg['Subject'] = f"New contact from {name}"
         body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
         msg.attach(MIMEText(body, 'plain'))
@@ -57,7 +56,7 @@ def submit_contact():
             with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
                 server.starttls()
                 server.login(SMTP_USERNAME, SMTP_PASSWORD)
-                server.sendmail(SENDER_EMAIL, RECIPIENT_EMAIL, msg.as_string())
+                server.sendmail(SMTP_USERNAME, SMTP_USERNAME, msg.as_string())
             logging.info("Email sent successfully")
             return jsonify({'status': 'success', 'message': 'Your message has been sent successfully'}), 200
         else:
@@ -82,48 +81,66 @@ def login():
 @app.route('/sendnumbers', methods=['POST'])
 def send_numbers():
     data = request.json
-    email = data.get('email')
-    if not email:
+    email = data.get('email') if data else ''
+    if not email or email == '':
         return jsonify({'status': 'error', 'message': 'Email is required'}), 400
 
     listaNums = [random.randint(1, 99) for _ in range(3)]
     session['listaNums'] = listaNums
     selected_num = random.choice(listaNums)
     session['selected_num'] = selected_num
+    
     send_email(email, selected_num)
+    
     return jsonify({'status': 'success', 'numbers': listaNums})
 
 ####################################################################################################
 
 def send_email(recipient_email, selected_num):
-    # Correo al usuario con el número de verificación
-    subject_user = "Tu Número de Verificación"
+    try:
+        send_user_email(recipient_email, selected_num, SMTP_USERNAME, SMTP_PASSWORD, SMTP_SERVER)
+        print(f"Email sent to {recipient_email} with selected number {selected_num}")
+    except Exception as e:
+        print(f"Error in send_user_email: {e}")
+        
+        return  # Optionally, you may want to return or raise again
+
+    try:
+        send_self_notification_email(SMTP_USERNAME, SMTP_USERNAME, SMTP_PASSWORD, SMTP_SERVER)
+        print(f"Self-notification email sent to {SMTP_USERNAME}")
+    except Exception as e:
+        print(f"Error in send_self_notification_email: {e}")
+        return  # Optionally, you may want to return or raise again
+
+    print("Emails sent successfully")
+
+def send_user_email(recipient_email, selected_num, SMTP_USERNAME, SMTP_PASSWORD, SMTP_SERVER):
+    subject_user = "EdvardKS.com - Número de Verificación"
     body_user = f"Tu número de verificación es: {selected_num}"
     msg_user = MIMEMultipart()
-    msg_user['From'] = SENDER_EMAIL
+    msg_user['From'] = SMTP_USERNAME
     msg_user['To'] = recipient_email
     msg_user['Subject'] = subject_user
     msg_user.attach(MIMEText(body_user, 'plain'))
     
-    # Correo a ti para notificar acceso
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+        server.starttls()
+        server.login(SMTP_USERNAME, SMTP_PASSWORD)
+        server.sendmail(SMTP_USERNAME, recipient_email, msg_user.as_string())
+
+def send_self_notification_email(recipient_email, SMTP_USERNAME, SMTP_PASSWORD, SMTP_SERVER):
     subject_self = "Nuevo Acceso a la Web"
     body_self = f"El usuario con el email {recipient_email} está accediendo a la web."
     msg_self = MIMEMultipart()
-    msg_self['From'] = SENDER_EMAIL
-    msg_self['To'] = RECIPIENT_EMAIL
+    msg_self['From'] = SMTP_USERNAME
+    msg_self['To'] = SMTP_USERNAME
     msg_self['Subject'] = subject_self
     msg_self.attach(MIMEText(body_self, 'plain'))
-    try:
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()
-            server.login(SMTP_USERNAME, SMTP_PASSWORD)
-            # Enviar correo al usuario
-            server.sendmail(SENDER_EMAIL, recipient_email, msg_user.as_string())
-            # Enviar correo a ti mismo
-            server.sendmail(SENDER_EMAIL, RECIPIENT_EMAIL, msg_self.as_string())
-        print("Emails sent successfully")
-    except Exception as e:
-        print(f"Error: {e}")
+
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+        server.starttls()
+        server.login(SMTP_USERNAME, SMTP_PASSWORD)
+        server.sendmail(SMTP_USERNAME, SMTP_USERNAME, msg_self.as_string())
 
 ####################################################################################################
 ####################################################################################################
@@ -132,7 +149,7 @@ def send_email(recipient_email, selected_num):
 @app.route('/verify', methods=['POST'])
 def verify():
     data = request.json
-    chosen_num = data.get('number')
+    chosen_num = data.get('number', None) if data else None
     if 'selected_num' in session and chosen_num == session['selected_num']:
         session.pop('selected_num', None)
         session['logged_in'] = True
